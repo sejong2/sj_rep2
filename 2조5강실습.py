a56d6a672151e71a75444a8eb53267d1623da4e0
sejong2 <semarykim@naver.com>
2017-04-11 21:50:35 +0900

    Add files via upload

---
+++ 2조5강실습.py
@@ -0,0 +1,260 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+3
+4
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+10
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+Enter the first input value : 10
+Enter the second input value : 20
+10  is less than  20
+10  is not equal to  20
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+Enter the first input value : 10
+Enter the second input value : 20
+10  is less than  20
+10  is not equal to  20
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+Enter the first input value : 35
+Enter the second input value : 17
+35  is equal to  17
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+Enter the first input value : 17
+Enter the second input value : 30
+17  is less than  30 .
+But multiple of  17  is greater than  30
+17  is not equal to  30
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+1 : Hello
+2 : Hello
+3 : Hello
+4 : Hello
+5 : Hello
+End of For Loop!!!
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+1 : Hello
+2 : Hello
+3 : Hello
+4 : Hello
+5 : Hello
+6 : Hello
+7 : Hello
+8 : Hello
+9 : Hello
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+5
+10
+15
+20
+25
+End of First Loop!!!
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+10
+9
+8
+7
+6
+5
+4
+3
+2
+End of Second Loop!!!
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+Happy New Year~~!!!
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+Happy New Year~~!!!
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+a  is an alphabet!
+b  is an alphabet!
+c  is an alphabet!
+d  is an alphabet!
+e  is an alphabet!
+f  is an alphabet!
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+Enter the first input value : 16
+Enter the second input value : 27
+16  is less than  27
+Enter the first input value : 27
+Enter the second input value : 16
+At last  27  is not less than  16
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+i =  1
+Can this line be printed?
+i =  2
+Can this line be printed?
+i =  3
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+i =  1
+Can this line be printed?
+i =  2
+Can this line be printed?
+i =  3
+i =  4
+Can this line be printed?
+i =  5
+Can this line be printed?
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+5  x  1  =  5
+5  x  2  =  10
+5  x  3  =  15
+5  x  4  =  20
+5  x  5  =  25
+5  x  6  =  30
+5  x  7  =  35
+5  x  8  =  40
+5  x  9  =  45
+====================
+6  x  1  =  6
+6  x  2  =  12
+6  x  3  =  18
+6  x  4  =  24
+6  x  5  =  30
+6  x  6  =  36
+6  x  7  =  42
+6  x  8  =  48
+6  x  9  =  54
+====================
+7  x  1  =  7
+7  x  2  =  14
+7  x  3  =  21
+7  x  4  =  28
+7  x  5  =  35
+7  x  6  =  42
+7  x  7  =  49
+7  x  8  =  56
+7  x  9  =  63
+====================
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+2  x  1  =  2
+2  x  2  =  4
+2  x  3  =  6
+2  x  4  =  8
+2  x  5  =  10
+2  x  6  =  12
+2  x  7  =  14
+2  x  8  =  16
+2  x  9  =  18
+====================
+3  x  1  =  3
+3  x  2  =  6
+3  x  3  =  9
+3  x  4  =  12
+3  x  5  =  15
+3  x  6  =  18
+3  x  7  =  21
+3  x  8  =  24
+3  x  9  =  27
+====================
+4  x  1  =  4
+4  x  2  =  8
+4  x  3  =  12
+4  x  4  =  16
+4  x  5  =  20
+4  x  6  =  24
+4  x  7  =  28
+4  x  8  =  32
+4  x  9  =  36
+====================
+5  x  1  =  5
+5  x  2  =  10
+5  x  3  =  15
+5  x  4  =  20
+5  x  5  =  25
+5  x  6  =  30
+5  x  7  =  35
+5  x  8  =  40
+5  x  9  =  45
+====================
+6  x  1  =  6
+6  x  2  =  12
+6  x  3  =  18
+6  x  4  =  24
+6  x  5  =  30
+6  x  6  =  36
+6  x  7  =  42
+6  x  8  =  48
+6  x  9  =  54
+====================
+7  x  1  =  7
+7  x  2  =  14
+7  x  3  =  21
+7  x  4  =  28
+7  x  5  =  35
+7  x  6  =  42
+7  x  7  =  49
+7  x  8  =  56
+7  x  9  =  63
+====================
+8  x  1  =  8
+8  x  2  =  16
+8  x  3  =  24
+8  x  4  =  32
+8  x  5  =  40
+8  x  6  =  48
+8  x  7  =  56
+8  x  8  =  64
+8  x  9  =  72
+====================
+9  x  1  =  9
+9  x  2  =  18
+9  x  3  =  27
+9  x  4  =  36
+9  x  5  =  45
+9  x  6  =  54
+9  x  7  =  63
+9  x  8  =  72
+9  x  9  =  81
+====================
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+How many stars do you want? 6
+* * * * * *
+>>> 
+============= RESTART: C:/Users/you-bin/Desktop/2조 5강 실습.py =============
+How many stars do you want? 6
+          *  
+        * * *  
+      * * * * *  
+    * * * * * * *  
+  * * * * * * * * *  
+* * * * * * * * * * *  
+>>> 
