2f078ef903a7abca39ca53a54ebd118e93ac7a74
sejong2 <semarykim@naver.com>
2017-05-02 14:30:06 +0900

    Add files via upload

---
+++ 7강 실습_.py
@@ -0,0 +1,26 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Users/you-bin/Desktop/7강 실습.py ===============
+red
+big
+down
+Call bounce method!
+up
+Call bounce method!
+down
+>>> print myBall.__dict__
+{'color': 'red', 'direction': 'down', 'size': 'big'}
+>>> 
+=============== RESTART: C:/Users/you-bin/Desktop/7강 실습.py ===============
+{'color': 'red', 'direction': 'down', 'size': 'big'}
+{'color': 'yellow', 'position': 'head', 'spin': None}
+{'color': 'yellow', 'position': 'head', 'spin': 'Topspin'}
+{'color': 'yellow', 'position': 'underneath', 'spin': 'Backspin'}
+{'color': 'yellow', 'position': 'underneath', 'direction': 'down', 'spin': 'Backspin'}
+>>> 
+=============== RESTART: C:/Users/you-bin/Desktop/7강 실습.py ===============
+Rover is of type Animal and has attributes:['beathe', 'runs', 'eats', 'dog']
+Rover is of type Dog and has attributes:['beathe', 'lazy', 'beige', 'sleeps', 'eats']
+Silver is of type Fox and has attributes:['beathe', 'quick', 'brown', 'jumps', 'runs']
+>>> 
